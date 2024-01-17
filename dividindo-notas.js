ae698e2158746e840c44163e91bd8389c6a62de1
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-01-16 21:04:32 -0300

    conectando com repositorio remoto

---
+++ dividindo-notas.js
@@ -0,0 +1,32 @@
+const alunos = [
+    "João",
+    "Juliana",
+    "Ana",
+    "Caio",
+    "Lara",
+    "Marjorie",
+    "Guilherme",
+    "Aline",
+    "Fabiana",
+    "Andre",
+    "Carlos",
+    "Paulo",
+    "Bia",
+    "Vivian",
+    "Isabela",
+    "Vinícius",
+    "Renan",
+    "Renata",
+    "Daisy",
+    "Camilo",
+];
+
+
+// acessar o array e seperar primeira lista de 0 a 9 e segunda 10 pra frente
+const sala1 = alunos.slice(0, alunos.length / 2);
+const sala2 = alunos.slice(alunos.length / 2);
+
+console.log(sala1);
+console.log(sala2);
+
+console.log(alunos);
